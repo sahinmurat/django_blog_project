bf3a22d4f2b219354c7be6944c3c3340b5ef6d98
Murat Sahin <yazilimogren1111@gmail.com>
2021-01-24 23:08:52 +0100

    bug fixes

--- src/user/urls.py
+++ src/user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterView
-from django.contrib.auth import views as auth_views
+from .views import RegisterView, Profile_get_update
+from django.contrib.auth import views as rest_views
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+    path('profile/<id>', Profile_get_update, name='profile'),
+    path('profile/update/', Profile_get_update, name='profile-update'),
   
 ]
